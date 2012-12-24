3a44e192801010cd45d9c3acaf69fe01e0054a80
Chris Rose <offby1@offby1.net>
2012-12-24 14:58:25 -0800

    Potential set of endpoints

--- randomizeme/views.py
+++ randomizeme/views.py
@@ -5,3 +5,23 @@ from randomizeme import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload-library')
+def upload_library():
+    pass
+
+@app.route('/submit-playlist')
+def submit_playlist():
+    pass
+
+@app.route('/get-playlist')
+def get_playlist():
+    pass
+
+@app.route('/library/{user}')
+def user_library(user):
+    pass
+
+@app.route('/manage-library')
+def my_libraries():
+    pass
